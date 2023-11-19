82edbb3b5967fae71846cd23148abbd4ba420599
Thomas Rainford <thomas@rainfords.net>
2023-11-20 11:03:59 +1300

    Fix 'module' property in package.json
    - Export LocalStorageValue type from index.ts.

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import type { LocalStorageValue } from "./src/useLocalStorage";
 import useLocalStorage from "./src/useLocalStorage";
+
+export { LocalStorageValue };
 
 export default useLocalStorage;
