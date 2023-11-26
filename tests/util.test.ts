c116bd329669b0f3d8e92d1acb181ecab0748230
Thomas Rainford <thomas@rainfords.net>
2023-11-26 15:28:14 +1300

    Add test suite for the 'util.ts' file.

---
+++ tests/util.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from "bun:test";
+import { isValidJson } from "../src/util";
+
+describe("util tests", () => {
+  describe("isValidJson test", () => {
+    test("returns true for valid JSON string", () => {
+      const input = JSON.stringify({ value: "test value" });
+      const result = isValidJson(input);
+      expect(result).toBeTruthy();
+    });
+
+    test("returns false for valid JSON string", () => {
+      const input = "invalid json";
+      const result = isValidJson(input);
+      expect(result).toBeFalsy();
+    });
+  });
+});
